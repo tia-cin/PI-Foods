8187f41c3eef957ea2bfc0f8e987cfb1c0e53ead
cintia <cintiaayelenarce@gmail.com>
2021-11-22 10:58:58 -0300

    Recipe Model

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,29 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id :{
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    score: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    health_score: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+    },
+    step_by_step: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
   });
 };
