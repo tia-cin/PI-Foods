588fc065cda293b22393d6488441f0c59cf7c51e
cintia <cintiaayelenarce@gmail.com>
2021-12-09 11:55:30 -0300

    done

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -21,22 +21,26 @@ router.get('/recipes', async (req, res) => {
     let { name } = req.query;
     let allRecipes = await getAllInfo();
     // console.log('apicall', allRecipes)
-    
-    // get recipe by name
-    if (name) {
-        let search = await allRecipes.filter(r => r.title.toLowerCase().includes(name))
-        if (search.length) return res.status(200).send(search)
-        else return res.status(404).send('No se encontró la receta')
+    try {
+        // get recipe by name
+        if (name) {
+            let search = await allRecipes.filter(r => r.title.toLowerCase().includes(name))
+            if (search.length) return res.status(200).send(search)
+            else return res.status(404).send('No se encontró la receta')
+        }
+        return res.status(200).send(allRecipes)
     }
-    
-    return res.status(200).send(allRecipes)
+    catch(error) {
+        console.log(error)
+    }
 } );
 
 
 // ruta para obtener una receta por id de la misma
 router.get('/recipes/:id', async (req, res) => {
     let { id } = req.params
-    if (id.length < 10) {
+    try {
+        if (id.length < 10) {
         let recipe = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`)
         return res.json({
                 id: recipe.data.id,
@@ -48,43 +52,53 @@ router.get('/recipes/:id', async (req, res) => {
                 summary: recipe.data.summary,
                 instructions: recipe.data.instructions
         })
-    }
-    else {
-        let recipe = await Recipe.findByPk(id, { include: Diet })
-        return res.json(recipe)
+        }
+        else {
+            let recipe = await Recipe.findByPk(id, { include: Diet })
+            return res.json(recipe)
+        }
+    } catch(error) {
+        console.log(error)
     }
 })
 
 
 // ruta para obtener los tipos de dietas
 router.get('/types', async (req, res) => {
-    let infoApi = await getAllInfo()
-    let diets = infoApi.map(d => d.diets)
-    let eachDiet = diets.map(d => {for (let i = 0; i < d.length; i++) return d[i]})
-    eachDiet.push('ketogenic', 'vegetarian', 'lacto vegetarian', 'ovo vegetarian', 'vegan', 'pescetarian', 'paleo', 'primal', 'low fodmap', 'whole30')
-    eachDiet.forEach(d => {
-        Diet.findOrCreate({ where: { name: d } })
-    });
-    
-    let allDiets = await Diet.findAll()
-    res.send(allDiets)
+    try {
+         let infoApi = await getAllInfo()
+        let diets = infoApi.map(d => d.diets)
+        let eachDiet = diets.map(d => {for (let i = 0; i < d.length; i++) return d[i]})
+        eachDiet.push('ketogenic', 'vegetarian', 'lacto vegetarian', 'ovo vegetarian', 'vegan', 'pescetarian', 'paleo', 'primal', 'low fodmap', 'whole30')
+        eachDiet.forEach(d => {
+            Diet.findOrCreate({ where: { name: d } })
+        });
+        
+        let allDiets = await Diet.findAll()
+        res.send(allDiets)
+    } catch(error) {
+        console.log(error)
+    }
 })
 
 
 // ruta para puclicar receta personalizada por el usuario
 router.post('/recipe', async (req, res) => {
-    let { name, summary, score, health_score, instructions, diets, createRecipe } = req.body;
-    let newRecipe = await Recipe.create({
-        name, 
-        summary,
-        score,
-        health_score,
-        instructions,
-        createRecipe
-    }) 
-    let recipeDiet = await Diet.findAll({ where: { name: diets } })
-    newRecipe.addDiet(recipeDiet)
-    res.send('Nueva Receta creada con éxito!')
+    let { name, summary, score, health_score, instructions, diets } = req.body;
+    try {
+         let newRecipe = await Recipe.findOrCreate({
+            name, 
+            summary,
+            score,
+            health_score,
+            instructions
+        }) 
+        let recipeDiet = await Diet.findAll({ where: { name: diets } })
+        newRecipe.addDiet(recipeDiet)
+        res.send('Nueva Receta creada con éxito!')
+    } catch(error) {
+        console.log(error)
+    }
 })
 
 module.exports = router;
